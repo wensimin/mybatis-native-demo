3a7e1d2a13279348aa55f8a767fdd5e614eaf3fc
wensimin <hukaizhiyu2@gmail.com>
2024-05-06 08:38:06 +0000

    增加lambda部分配置&补充配置

--- src/main/java/com/example/nativedemo/LambdaRegistrationFeature.java
+++ src/main/java/com/example/nativedemo/LambdaRegistrationFeature.java
@@ -1,20 +1,20 @@
 package com.example.nativedemo;
 
 
+import com.example.nativedemo.controller.DemoController;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeSerialization;
 
 /**
  * lambda 表达式注入到graal中
  * @author ztp
- * @date 2023/8/18 11:53
  */
 public class LambdaRegistrationFeature implements Feature {
 
     @Override
     public void duringSetup(DuringSetupAccess access) {
         // TODO 这里需要将lambda表达式所使用的成员类都注册上来,具体情况视项目情况而定,一般扫描@Controller和@Service的会多点.
-        RuntimeSerialization.registerLambdaCapturingClass(NativeDemoApplication.class);
+        RuntimeSerialization.registerLambdaCapturingClass(DemoController.class);
     }
 
 }
